bf44ffa1d9d08ee6336f03005de54b5ca1ef8800
Peter Kese <peter.kese@tobonet.com>
2021-06-02 10:29:52 +0200

    Upgrade to dotnet 5

--- build.sh
+++ build.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-if [ ! -f .paket/paket ]; then
+if ! dotnet tool list | grep -q "^paket" ; then
     echo installing paket
-    dotnet tool install Paket --tool-path .paket --version [5.249.2]
+    dotnet tool restore
+    #dotnet tool install Paket --tool-path .paket --version 5.257.0
+
+    # it would be best to just
+    # - keep tool versions in .config/dotnet-tools.json
+    # - run `dotnet tool restore` unconditionally to make sure specified version is installed
 fi
 
 if [ ! -f paket.lock ]; then
     echo 'running paket install (no paket.lock found)'
-    .paket/paket install
+    #.paket/paket install
+    dotnet paket install
 fi
 
 if [ ! -f packages/build/fake-cli/tools/netcoreapp2.1/any/fake-cli.dll ]; then
     echo running paket restore
-    .paket/paket install
+    #.paket/paket install
+    dotnet paket install
 fi
-
-dotnet tool restore
 
 dotnet packages/build/fake-cli/tools/netcoreapp2.1/any/fake-cli.dll build $@ 
